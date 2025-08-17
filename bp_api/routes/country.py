12e9b4f704a47d1978d4f6d6841ba394c475db4f
Stan Mertens <stnmertens@gmail.com>
2025-08-17 13:50:45 +0200

    drop web_

---
+++ bp_api/routes/country.py
@@ -0,0 +1,96 @@
+from web.api import API, json_response
+from web.auth import authorize
+from web.database import conn
+from web.database.model import Country, UserRoleLevel
+from werkzeug import Response
+
+from bp_api import api_bp
+
+#
+# Configuration
+#
+
+
+class CountryAPI(API):
+    model = Country
+    post_columns = {
+        Country.code,
+        Country.in_sitemap,
+        Country.name,
+        Country.currency_id,
+        Country.region_id,
+    }
+    patch_columns = {
+        Country.state_required,
+        Country.vat_required,
+    }
+    get_columns = {
+        Country.code,
+        Country.in_sitemap,
+        Country.name,
+        Country.id,
+        Country.currency_id,
+        Country.region_id,
+    }
+
+
+#
+# Endpoints
+#
+
+
+@api_bp.post("/countries")
+@authorize(UserRoleLevel.ADMIN)
+def post_countries() -> Response:
+    api = CountryAPI()
+    data = api.gen_data(api.post_columns)
+    with conn.begin() as s:
+        model = api.model()
+        api.insert(s, data, model)
+        resource = api.gen_resource(s, model)
+    return json_response(data=resource)
+
+
+@api_bp.get("/countries")
+def get_countries() -> Response:
+    api = CountryAPI()
+    with conn.begin() as s:
+        models: list[Country] = api.list_(s, order_by=Country.name)
+        resources = api.gen_resources(s, models)
+    return json_response(data=resources)
+
+
+@api_bp.get("/countries/<int:country_id>")
+def get_countries_id(country_id: int) -> Response:
+    api = CountryAPI()
+    with conn.begin() as s:
+        model: Country = api.get(s, country_id)
+        resource = api.gen_resource(s, model)
+    return json_response(data=resource)
+
+
+@api_bp.patch("/countries/<int:country_id>")
+@authorize(UserRoleLevel.ADMIN)
+def patch_countries_id(country_id: int) -> Response:
+    api = CountryAPI()
+    data = api.gen_data(api.patch_columns)
+    with conn.begin() as s:
+        model: Country = api.get(s, country_id)
+        api.update(s, data, model)
+        resource = api.gen_resource(s, model)
+    return json_response(data=resource)
+
+
+@api_bp.delete("/countries/<int:country_id>")
+@authorize(UserRoleLevel.ADMIN)
+def delete_countries_id(country_id: int) -> Response:
+    api = CountryAPI()
+    with conn.begin() as s:
+        model: Country = api.get(s, country_id)
+        api.delete(s, model)
+    return json_response()
+
+
+#
+# Functions
+#
---
+++ bp_api/routes/settings.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timezone
+
+from sqlalchemy.orm import Session
+from web.api import API, json_response
+from web.auth import authorize
+from web.database import conn
+from web.database.model import AppSettings, UserRoleLevel
+from werkzeug import Response
+
+from bp_api import api_bp
+
+#
+# Configuration
+#
+
+
+class SettingsAPI(API):
+    model = AppSettings
+    patch_columns = {
+        AppSettings.banner,
+        AppSettings.cached_at,
+    }
+    get_columns = {
+        AppSettings.banner,
+        AppSettings.cached_at,
+    }
+
+
+#
+# Endpoints
+#
+
+
+@api_bp.get("/settings")
+@authorize(UserRoleLevel.ADMIN)
+def get_settings() -> Response:
+    api = SettingsAPI()
+    with conn.begin() as s:
+        model: AppSettings = api.get(s, None)
+        resource = api.gen_resource(s, model)
+    return json_response(data=resource)
+
+
+@api_bp.patch("/settings")
+@authorize(UserRoleLevel.ADMIN)
+def patch_settings() -> Response:
+    api = SettingsAPI()
+    data = api.gen_data(api.patch_columns)
+    with conn.begin() as s:
+        model: AppSettings = api.get(s, None)
+        set_cache(s, data, model)
+        api.update(s, data, model)
+        resource = api.gen_resource(s, model)
+    return json_response(data=resource)
+
+
+#
+# Functions
+#
+
+
+def set_cache(s: Session, data: dict, model: AppSettings) -> None:
+    if "cached_at" in data:
+        data["cached_at"] = datetime.now(timezone.utc).isoformat()
